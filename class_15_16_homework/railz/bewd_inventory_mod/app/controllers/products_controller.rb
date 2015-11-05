383f92a37ba883b674202c50f79df180d7a6b917
lskd <akonif@gmail.com>
2015-11-04 17:39:45 -0800

    modifications to bewd inventory example

--- class_15_16_homework/railz/bewd_inventory_mod/app/controllers/products_controller.rb
+++ class_15_16_homework/railz/bewd_inventory_mod/app/controllers/products_controller.rb
@@ -19,21 +19,29 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-  # POST /products
-  # POST /products.json
+
   def create
-    @product = Product.new(product_params)
-
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+      @product = Product.new(product_params)
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to @product, notice: 'Added like a ballon'}
+        else
+          format.html { render :new}
+        end
       end
-    end
+
+  #   @product = Product.new(product_params)
+  #
+  # respond_to do |format|
+  #   if @product.save
+  #     format.html { redirect_to @product, notice: 'Product was successfully created.' }
+  #     format.json { render :show, status: :created, location: @product }
+  #   else
+  #     format.html { render :new }
+  #     format.json { render json: @product.errors, status: :unprocessable_entity }
+  #   end
+  # end
+
   end
 
 
--- class_15_16_homework/railz/bewd_inventory_mod/config/routes.rb
+++ class_15_16_homework/railz/bewd_inventory_mod/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'products#index' # may need to add a href or routes and or recomment this out
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
